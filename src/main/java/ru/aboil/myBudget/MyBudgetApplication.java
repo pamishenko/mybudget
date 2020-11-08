49724b9fed0b06371a9cdfdd7a9a5d3db57986de
pamishenko <pamishenko@gmail.com>
2020-11-09 00:55:29 +0300

    Создал проект заново в spring boot

---
+++ src/main/java/ru/aboil/myBudget/MyBudgetApplication.java
@@ -0,0 +1,13 @@
+package ru.aboil.myBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBudgetApplication.class, args);
+	}
+
+}
